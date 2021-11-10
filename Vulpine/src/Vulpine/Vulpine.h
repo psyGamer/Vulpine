3bb06b29da2427ff1f3ea4f7522d3bd96926d338
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-11-10 18:31:04 +0100

    Added header file for users

---
+++ Vulpine/src/Vulpine/Vulpine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// temporary..
+#include "Renderer/Vulkan/Commands.h"
+#include "Renderer/Vulkan/Device.h"
+#include "Renderer/Vulkan/Instance.h"
+#include "Renderer/Vulkan/Pipeline.h"
+#include "Renderer/Vulkan/RenderPass.h"
+#include "Renderer/Vulkan/Shader.h"
+#include "Renderer/Vulkan/Swapchain.h"
+#include "Renderer/Vulkan/VertexBuffer.h"
