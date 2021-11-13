b87b5ff6276f1f4a2a3c792cf693b2e2305b2b07
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-11-13 16:57:17 +0100

    Updated Sandbox

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -27,18 +27,63 @@ void MAIN()
 	Swapchain::Create();
 
 	Shader vertShader("src/shader.vert.spv", Shader::Type::VERTEX);
-	Shader fragShader("src/shader.frag.spv", Shader::Type::VERTEX);
+	Shader fragShader("src/shader.frag.spv", Shader::Type::FRAGMENT);
 
 	VertexBuffer vertBuffer(0, {
 		VertexAttributeValue::F32VEC2
 	});
 
 	Pipeline pipeline(vertShader, fragShader, vertBuffer);
+	pipeline.Create();
 
-	while (!glfwWindowShouldClose(Vulpine::Window::GetWindow())) glfwWaitEvents();
+	vertShader.Destroy();
+	fragShader.Destroy();
+
+	CommandPool::Create();
+	CommandBuffers::Create();
+	CommandBuffers::Record(pipeline);
+
+	Semaphore imageAvailableSemaphore;
+	Semaphore renderFinishedSemaphore;
+
+	while (!glfwWindowShouldClose(Vulpine::Window::GetWindow()))
+	{
+		glfwWaitEvents();
+
+		uint32_t imageIndex = Swapchain::AcquireNextImageIndex(imageAvailableSemaphore);
+		CommandBuffers::Submit(imageIndex, imageAvailableSemaphore, renderFinishedSemaphore);
+
+		VkPresentInfoKHR presentInfo{};
+		presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+
+		VkSemaphore renderFinished = renderFinishedSemaphore.GetSemaphore();
+
+		presentInfo.waitSemaphoreCount = 1;
+		presentInfo.pWaitSemaphores = &renderFinished;
+
+		VkSwapchainKHR swapChains[] = { Swapchain::GetSwapchain() };
+
+		presentInfo.swapchainCount = 1;
+		presentInfo.pSwapchains = swapChains;
+		presentInfo.pImageIndices = &imageIndex;
+
+		presentInfo.pResults = nullptr;
+
+		vkQueuePresentKHR(Device::GetPresentQueue(), &presentInfo);
+	}
+
+	vkDeviceWaitIdle(Device::GetLogicalDevice());
+
+	imageAvailableSemaphore.Destroy();
+	renderFinishedSemaphore.Destroy();
+
+	CommandBuffers::Destroy();
+	CommandPool::Destroy();
+
+	pipeline.Destroy();
 
 	Swapchain::Destroy();
 	Instance::Destory();
 
-	Vulpine::Window::Destroy();
+	Window::Destroy();
 }
