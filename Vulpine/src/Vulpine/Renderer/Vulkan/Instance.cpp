ca8556bf997764eefce02c943e753ca1768b56b3
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-11-13 09:44:45 +0100

    Added debug messenger

--- Vulpine/src/Vulpine/Renderer/Vulkan/Instance.cpp
+++ Vulpine/src/Vulpine/Renderer/Vulkan/Instance.cpp
@@ -5,13 +5,48 @@
 
 #include "Device.h"
 
+#ifdef VP_DEBUG
+#define VULKAN_VALIDATION
+#endif
+
 namespace Vulpine::Vulkan
 {
 	VkInstance Instance::s_Instance = VK_NULL_HANDLE;
 
+#ifdef VULKAN_VALIDATION
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData)
+	{
+		std::string message;
+
+		if (messageType == VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT)
+			message = "(Performance) " + std::string(pCallbackData->pMessage);
+		else if (messageType == VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT)
+			message = std::string(pCallbackData->pMessage);
+		else
+			message = "(General) " + std::string(pCallbackData->pMessage);
+
+		if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)
+			VP_ERROR(message);
+		else if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+			VP_WARN(message);
+		else if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT)
+			VP_INFO(message);
+		else if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT)
+			VP_TRACE(message);
+
+		return VK_FALSE;
+	}
+
+	static VkDebugUtilsMessengerEXT debugMessenger;
+#endif
+
 	void Instance::Create()
 	{
-		VP_ASSERT(Window::GetWindow != nullptr, "Window not yet initialized");
+		VP_ASSERT(Window::GetWindow() == nullptr, "Window not yet initialized!");
 
 		// Application Info
 	
@@ -28,7 +63,7 @@ namespace Vulpine::Vulkan
 		// Instance Info
 
 		const std::vector<const char*> instanceLayers = {
-#ifdef _DEBUG
+#ifdef VULKAN_VALIDATION
 		"VK_LAYER_KHRONOS_validation"
 #endif
 		};
@@ -36,23 +71,67 @@ namespace Vulpine::Vulkan
 		uint32_t glfwExtensionCount;
 		auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
+#ifdef VULKAN_VALIDATION
+		std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+		VkDebugUtilsMessengerCreateInfoEXT debugMessengerInfo{};
+		debugMessengerInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+
+		debugMessengerInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		debugMessengerInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		debugMessengerInfo.pfnUserCallback = debugCallback;
+#endif
+
 		VkInstanceCreateInfo instanceInfo{};
 		instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+#ifdef VULKAN_VALIDATION
+		instanceInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugMessengerInfo;
+#endif
 
 		instanceInfo.pApplicationInfo = &applicationInfo;
 
 		instanceInfo.enabledLayerCount = instanceLayers.size();
 		instanceInfo.ppEnabledLayerNames = instanceLayers.data();
+#ifdef VULKAN_VALIDATION
+		instanceInfo.enabledExtensionCount = extensions.size();
+		instanceInfo.ppEnabledExtensionNames = extensions.data();
+#else
 		instanceInfo.enabledExtensionCount = glfwExtensionCount;
 		instanceInfo.ppEnabledExtensionNames = glfwExtensions;
+#endif
 
 		VP_ASSERT_VK(vkCreateInstance(&instanceInfo, nullptr, &s_Instance), "Failed to create instance");
+
+#ifdef VULKAN_VALIDATION
+		auto createDebugUtilsMessengerFunc = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(s_Instance, "vkCreateDebugUtilsMessengerEXT");
+
+		if (createDebugUtilsMessengerFunc != nullptr)
+		{
+			VP_ASSERT_VK(createDebugUtilsMessengerFunc(s_Instance, &debugMessengerInfo, nullptr, &debugMessenger), "Failed to create debug messenger!");
+		}
+		else
+		{
+			VP_ERROR("Debug messenger extensions is not present!");
+		}
+#endif
+		Window::CreateSurface(s_Instance);
 
 		Device::Create(s_Instance);
 	}
 
 	void Instance::Destory()
 	{
+#ifdef VULKAN_VALIDATION
+		auto destroyDebugUtilsMessengerFunc = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(s_Instance, "vkDestroyDebugUtilsMessengerEXT");
+		if (destroyDebugUtilsMessengerFunc != nullptr)
+		{
+			destroyDebugUtilsMessengerFunc(s_Instance, debugMessenger, nullptr);
+		}
+#endif
+
 		vkDestroyInstance(s_Instance, nullptr);
 	}
 }
