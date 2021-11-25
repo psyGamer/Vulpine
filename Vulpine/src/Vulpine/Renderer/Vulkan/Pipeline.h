dc5a273ed5f9e72cf0fc902d66b6f22d4c23c18e
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-11-25 20:44:28 +0100

    Added `IndexBuffer` integration in `Pipeline`

--- Vulpine/src/Vulpine/Renderer/Vulkan/Pipeline.h
+++ Vulpine/src/Vulpine/Renderer/Vulkan/Pipeline.h
@@ -6,6 +6,7 @@
 
 #include "Shader.h"
 #include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 namespace Vulpine::Vulkan
 {
@@ -19,6 +20,7 @@ namespace Vulpine::Vulkan
 		void Destroy();
 
 		void SetVertexBuffer(const VertexBuffer& vertexBuffer);
+		void SetIndexBuffer(const IndexBuffer& indexBuffer);
 
 		void ResetViewport();
 		void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height);
@@ -29,6 +31,7 @@ namespace Vulpine::Vulkan
 		VkRect2D GetScissor() const { return m_Scissor; }
 
 		Reference<const VertexBuffer> GetVertexBuffer() const { return m_pVertexBuffer; }
+		Reference<const IndexBuffer> GetIndexBuffer() const { return m_pIndexBuffer; }
 
 		const VkPipeline& GetPipeline() const { return m_Pipeline; }
 		const VkPipelineLayout& GetPipelineLayout() const { return m_PipelineLayout; }
@@ -41,6 +44,7 @@ namespace Vulpine::Vulkan
 		Shader m_FragmentShader;
 
 		Reference<const VertexBuffer> m_pVertexBuffer;
+		Reference<const IndexBuffer> m_pIndexBuffer;
 
 		VkViewport m_Viewport;
 		VkRect2D m_Scissor;
