e09c55d3263e33cf8da42e3eeb4c1113bf4f7541
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-11-26 16:27:10 +0100

    Converted `VertexAttribute` into `DataType`

--- Vulpine/src/Vulpine/Renderer/Vulkan/VertexBuffer.cpp
+++ Vulpine/src/Vulpine/Renderer/Vulkan/VertexBuffer.cpp
@@ -6,20 +6,20 @@ namespace Vulpine::Vulkan
 	VertexBuffer::VertexBuffer()
 		: m_pBuffer(nullptr)
 	{ }
-	VertexBuffer::VertexBuffer(const std::initializer_list<VertexAttribute>& vertexAttributes, uint32_t vertexCount)
+	VertexBuffer::VertexBuffer(const std::initializer_list<DataType>& vertexAttributes, uint32_t vertexCount)
 		: m_pBuffer(nullptr)
 	{
 		SetLayout(vertexAttributes, vertexCount);
 	}
 
-	void VertexBuffer::SetLayout(const std::initializer_list<VertexAttribute>& vertexAttributes, uint32_t vertexCount)
+	void VertexBuffer::SetLayout(const std::initializer_list<DataType>& vertexAttributes, uint32_t vertexCount)
 	{
 		m_VertexAttributes = vertexAttributes;
 
 		size_t size = 0;
 
 		for (const auto& vertexAttribute : vertexAttributes)
-			size += QueryVertexAttributeSize(vertexAttribute);
+			size += DataTypeHelper::QueryDataTypeSize(vertexAttribute);
 		size *= vertexCount;
 
 		m_pBuffer = CreateReference<GpuBuffer>(size,
@@ -40,7 +40,7 @@ namespace Vulpine::Vulkan
 		uint32_t stride = 0;
 
 		for (const auto& vertexAttribute : m_VertexAttributes)
-			stride += QueryVertexAttributeSize(vertexAttribute);
+			stride += DataTypeHelper::QueryDataTypeSize(vertexAttribute);
 
 		VkVertexInputBindingDescription bindingDescription{};
 		bindingDescription.binding = bindingIndex;
@@ -61,106 +61,10 @@ namespace Vulpine::Vulkan
 			VkVertexInputAttributeDescription& attributeDescription = attributeDescriptionsBuffer->emplace_back();
 			attributeDescription.binding = bindingIndex;
 			attributeDescription.location = location++;
-			attributeDescription.format = QueryVertexAttributeFormat(vertexAttribute);
+			attributeDescription.format = DataTypeHelper::QueryDataTypeFormat(vertexAttribute);
 			attributeDescription.offset = offset;
 
-			offset += QueryVertexAttributeSize(vertexAttribute);
-		}
-	}
-
-	uint32_t VertexBuffer::QueryVertexAttributeSize(const VertexAttribute& vertexAttribute)
-	{
-		switch (vertexAttribute)
-		{
-		case VertexAttribute::INT8:
-		case VertexAttribute::UINT8:
-			return 1;
-			
-		case VertexAttribute::INT16:
-		case VertexAttribute::UINT16:
-			return 2;
-
-		case VertexAttribute::INT32:
-		case VertexAttribute::UINT32:
-		case VertexAttribute::FLOAT32:
-			return 4;
-
-		case VertexAttribute::FLOAT64:
-			return 8;
-
-		case VertexAttribute::I32VEC2:
-		case VertexAttribute::F32VEC2:
-		case VertexAttribute::U32VEC2:
-			return 4 * 2;
-		case VertexAttribute::I32VEC3:
-		case VertexAttribute::F32VEC3:
-		case VertexAttribute::U32VEC3:
-			return 4 * 3;
-		case VertexAttribute::U32VEC4:
-		case VertexAttribute::F32VEC4:
-		case VertexAttribute::I32VEC4:
-			return 4 * 4;
-
-		case VertexAttribute::F64VEC2:
-			return 8 * 2;
-		case VertexAttribute::F64VEC3:
-			return 8 * 3;
-		case VertexAttribute::F64VEC4:
-			return 8 * 4;
-		}
-	}
-
-	VkFormat VertexBuffer::QueryVertexAttributeFormat(const VertexAttribute& vertexAttribute)
-	{
-		switch (vertexAttribute)
-		{
-		case VertexAttribute::INT8:
-			return VK_FORMAT_R8_SINT;
-		case VertexAttribute::UINT8:
-			return VK_FORMAT_R8_UINT;
-
-		case VertexAttribute::INT16:
-			return VK_FORMAT_R16_SINT;
-		case VertexAttribute::UINT16:
-			return VK_FORMAT_R16_UINT;
-
-		case VertexAttribute::INT32:
-			return VK_FORMAT_R32_SINT;
-		case VertexAttribute::UINT32:
-			return VK_FORMAT_R32_UINT;
-
-		case VertexAttribute::FLOAT32:
-			return VK_FORMAT_R32_SFLOAT;
-		case VertexAttribute::FLOAT64:
-			return VK_FORMAT_R64_SFLOAT;
-
-		case VertexAttribute::I32VEC2:
-			return VK_FORMAT_R32G32_SINT;
-		case VertexAttribute::U32VEC2:
-			return VK_FORMAT_R32G32_UINT;
-		case VertexAttribute::F32VEC2:
-			return VK_FORMAT_R32G32_SFLOAT;
-
-		case VertexAttribute::I32VEC3:
-			return VK_FORMAT_R32G32B32_SINT;
-		case VertexAttribute::U32VEC3:
-			return VK_FORMAT_R32G32B32_UINT;
-		case VertexAttribute::F32VEC3:
-			return VK_FORMAT_R32G32B32_SFLOAT;
-
-		case VertexAttribute::I32VEC4:
-			return VK_FORMAT_R32G32B32A32_SINT;
-		case VertexAttribute::U32VEC4:
-			return VK_FORMAT_R32G32B32A32_UINT;
-		case VertexAttribute::F32VEC4:
-			return VK_FORMAT_R32G32B32A32_SFLOAT;
-
-		case VertexAttribute::F64VEC2:
-			return VK_FORMAT_R64G64_SFLOAT;
-		case VertexAttribute::F64VEC3:
-			return VK_FORMAT_R64G64B64_SFLOAT;
-		case VertexAttribute::F64VEC4:
-			return VK_FORMAT_R64G64B64A64_SFLOAT;
+			offset += DataTypeHelper::QueryDataTypeSize(vertexAttribute);
 		}
 	}
 }
