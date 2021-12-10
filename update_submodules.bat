9fbd4511d437e41ef80b67d2e1520af74cf8bfbb
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-12-10 21:54:51 +0100

    Added batch file to update submodules

---
+++ update_submodules.bat
@@ -0,0 +1,21 @@
+@ECHO off
+
+pushd Vulpine\vendor
+
+pushd benchmark
+git pull origin main
+popd
+
+pushd spdlog
+git pull origin v1.x
+popd
+
+pushd glfw
+git pull origin master
+popd
+
+pushd glm
+git pull origin master
+popd
+
+popd
